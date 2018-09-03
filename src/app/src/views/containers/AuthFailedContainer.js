036917d8143757c2002555d0734800a3165da544
Maxim Kovalov <max.kovalov@gmail.com>
2018-09-03 23:33:09 +0300

    feat(wallets-ui): integrate select wallet

--- src/app/src/views/containers/AuthFailedContainer.js
+++ src/app/src/views/containers/AuthFailedContainer.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { LWSAuthError } from 'selfkey-ui';
+import { appSelectors } from '../../state/app';
+import { push } from 'react-router-redux';
 
 class AuthFailedContainer extends Component {
+	handleRetry = evt => {
+		evt.preventDefault();
+		const { config } = this.props;
+		this.props.dispatch(push(`${config.hash}/wallets`));
+	};
 	render() {
-		return <div>Auth Failed</div>;
+		return <LWSAuthError website={this.props.config.website} retryAction={this.handleRetry} />;
 	}
 }
 
-const ConnectedAuthFailedContainer = connect()(AuthFailedContainer);
+const ConnectedAuthFailedContainer = connect(state => ({
+	config: appSelectors.getAppConfig(state)
+}))(AuthFailedContainer);
 
 export default ConnectedAuthFailedContainer;
--- src/app/src/views/containers/WalletsContainer.js
+++ src/app/src/views/containers/WalletsContainer.js
@@ -1,82 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { walletsOperations, walletsSelectors } from '../../state/wallets';
+import { LWSSelectWallet } from 'selfkey-ui';
 
 class WalletsContainer extends Component {
 	componentDidMount() {
 		const { dispatch } = this.props;
 		dispatch(walletsOperations.loadWallets());
 	}
-	handleUnlock = (publicKey, password) => {
+	handleLogin = (publicKey, password) => {
 		this.props.dispatch(walletsOperations.unlockWallet(publicKey, password));
 	};
-	handleSelect = wallet => {
-		this.props.dispatch(walletsOperations.selectWallet(wallet.publicKey));
-	};
 	render() {
-		const { selectedWallet, wallets } = this.props;
-		console.log('render', selectedWallet, wallets);
-		let html = selectedWallet ? (
-			<WalletForm selectedWallet={selectedWallet} onUnlock={this.handleUnlock} />
-		) : (
-			<WalletSelect wallets={wallets} onSelect={this.handleSelect} />
-		);
-
-		return (
-			<div>
-				Wallets
-				{html}
-			</div>
-		);
+		const { wallets } = this.props;
+		return <LWSSelectWallet wallets={wallets} loginAction={this.handleLogin} />;
 	}
 }
-
-class WalletForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { password: '' };
-	}
-
-	handlePasswordChange = evt => {
-		this.setState({ password: evt.target.value });
-	};
-	handleUnlockClick = evt => {
-		evt.preventDefault();
-		this.props.onUnlock(this.props.selectedWallet.publicKey, this.state.password);
-	};
-
-	render() {
-		const { selectedWallet } = this.props;
-		return (
-			<div>
-				public key : {selectedWallet.publicKey} <br />
-				password:{' '}
-				<input
-					type="password"
-					onChange={this.handlePasswordChange}
-					value={this.state.password}
-				/>
-				<br />
-				<button onClick={this.handleUnlockClick}>unlock</button>
-			</div>
-		);
-	}
-}
-
-const WalletSelect = ({ wallets, onSelect }) => {
-	const handleWalletSelect = wallet => evt => {
-		evt.preventDefault();
-		onSelect(wallet);
-	};
-
-	let walletsHtml = wallets.map(w => (
-		<li key={w.publicKey} onClick={handleWalletSelect(w)}>
-			{w.publicKey}
-		</li>
-	));
-
-	return <ul>{walletsHtml}</ul>;
-};
 
 const ConnectedWalletsContainer = connect(state => ({
 	wallets: walletsSelectors.getWallets(state),
