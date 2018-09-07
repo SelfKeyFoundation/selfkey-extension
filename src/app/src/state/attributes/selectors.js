02adc65b2d2075b580ac14ae6199b8384cb9d123
Maxim Kovalov <max.kovalov@gmail.com>
2018-09-07 20:07:54 +0300

    feat(disallowd): filter out empty fields from auth

--- src/app/src/state/attributes/selectors.js
+++ src/app/src/state/attributes/selectors.js
@@ -1,8 +1,15 @@
 export const getAttributes = ({ attributes }) => attributes.list.map(key => attributes.byKey[key]);
 export const getLoading = ({ attributes }) => attributes.loading;
 export const getDisallowed = ({ attributes }) => {
-	console.log(attributes);
 	return attributes.disallowed.map(key => attributes.byKey[key]);
 };
 export const getOnlyAllowed = ({ attributes }) =>
-	attributes.list.filter(k => attributes.disallowed.indexOf(k) < 0).map(k => attributes.byKey[k]);
+	attributes.list
+		.filter(k => {
+			let attr = attributes.byKey[k];
+			if (!attr.data || Object.keys(attr.data).length === 0) {
+				return false;
+			}
+			return attributes.disallowed.indexOf(k) < 0;
+		})
+		.map(k => attributes.byKey[k]);
