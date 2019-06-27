a77ba61deb0483a193505ea0cac79c6d2a065d3f
Maxim Kovalov <max.kovalov@gmail.com>
2019-06-27 13:13:28 +0300

    feat(wallets): always try to auth first

--- src/app/src/state/wallets/selectors.js
+++ src/app/src/state/wallets/selectors.js
@@ -1,6 +1,9 @@
 export const getWallets = ({ wallets }) => wallets.list.map(publicKey => wallets.byKey[publicKey]);
 
-export const getOneWallet = ({ wallets }, publicKey) => wallets.byKey[publicKey];
+export const getOneWallet = ({ wallets }, publicKey) => ({
+	...wallets.byKey[publicKey],
+	signedUp: true
+});
 
 export const getSelected = state =>
 	state.wallets.selected ? getOneWallet(state, state.wallets.selected) : null;
